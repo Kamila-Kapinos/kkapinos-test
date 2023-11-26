d0182148906aac12a1c2afb6569948834f6e0f9f
Kamila Kapinos <s221572@student.uek.krakow.pl>
2023-11-26 21:03:45 +0000

    some new scripts

--- ex2/install_java_app.sh
+++ ex2/install_java_app.sh
@@ -3,16 +3,18 @@
 MY_NAME=${MY_NAME:-"Kamila Kapinos"}
 APP_URL=${APP_URL:-"https://github.com/Kamila-Kapinos/e-commerce/releases/download/v1.11/my-ecommerce-0.1.jar"}
 APP_DIR=/opt/ecommerce
+APP_NAME=${APP_NAME:-ecommerce}
 
 ## https://github.com/jkanclerz/computer-programming-4/releases/download/v1.29/my-ecommerce-0.1.jar
 ## https://github.com/Kamila-Kapinos/e-commerce/releases/download/v1.11/my-ecommerce-0.1.jar
+
 echo "hello ${MY_NAME}"
 
 PACKAGES_TO_BE_INSTALLED="mc cowsay tree"
 dnf install -y -q ${PACKAGES_TO_BE_INSTALLED}
 
 ## Install java
-dnf install -y java-17-amazon-corretto
+dnf install -y -q java-17-amazon-corretto
 
 mkdir -p ${APP_DIR}
 
@@ -20,7 +22,32 @@ mkdir -p ${APP_DIR}
 
 wget ${APP_URL} -O ${APP_DIR}/app.jar
 
-# java -Dservwe.port=80 -jar ${APP_DIR}/app.jar
+# java -Dserver.port=80 -jar ${APP_DIR}/app.jar
+
+##configure systemd
+## /ect/systemd/system/ecommerce.service
+
+serviceTemplate="
+[Unit]
+Description=${APP_NAME}
+After=network-online.target
+
+[Service]
+Type=simple
+User=root
+ExecStart=java -Dserver.port=80 -jar ${APP_DIR}/app.jar
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+"
+
+echo -e "$serviceTemplate" > /etc/systemd/system/${APP_NAME}.service
+
+systemctl daemon-reload
+systemctl enable ${APP_NAME}
+systemctl restart ${APP_NAME}
+
 
 
 cowsay "it works"
